174c76955885c93dbc9df135465157703176121d
Salchii <lupanchox@gmail.com>
2022-04-27 13:29:30 -0300

    make it better

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
 const routeRecipe = require('./recipes')
 const routeDiet = require('./diets')
+const cors = require('cors')
 
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 const router = Router()
+router.use(cors())
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
